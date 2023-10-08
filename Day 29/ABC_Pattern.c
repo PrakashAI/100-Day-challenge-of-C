870cc77d983c704d3f1299f6b9af1a39a5d74e27
PrakashAI <137335139+PrakashAI@users.noreply.github.com>
2023-10-09 01:41:48 +0530

    ABC_Pattern.c

---
+++ Day 29/ABC_Pattern.c
@@ -0,0 +1,25 @@
+/*output
+A
+AB
+ABC
+ABCD
+ABCDE
+
+
+
+*/
+
+#include<stdio.h>
+
+int main(){int x;
+printf("Enter the value");
+scanf("%d",&x);
+for(int i=0;i<x;i++){
+for(int j=0;j<i;j++){
+ 
+  printf("%c",65+j);}
+
+
+printf("\n");}
+return 0;
+   }
